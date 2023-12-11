a611caa5ce94078efaab5ee287c9562632fe5f9d
Luis Roa <luis.roa@monday-consulting.com>
2023-12-11 17:04:53 +0100

    remove unnecessary logging

--- services/router.js
+++ services/router.js
@@ -16,7 +16,6 @@ const Router = {
     Router.go(location.pathname);
   },
   go: (route, addToHistory=true) => {
-    console.log(`Going to ${route}`);
     if (addToHistory) {
       history.pushState({ route }, null, route);
     }
